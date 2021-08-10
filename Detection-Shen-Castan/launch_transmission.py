e636341f4e27c598a6358c8c262d75f3eb6bce4f
mathildevergnaud <55981984+mathildevergnaud@users.noreply.github.com>
2021-08-10 12:10:35 +0200

    Create launch_transmission.py

---
+++ Detection-Shen-Castan/launch_transmission.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+
+import Transmission
+ 
+#path = "tiles_acquisition_stitcher2.czi"
+path = "tiles_acquisition_stitcher0.czi"
+
+image_trans = Transmission.Transmission(path)
+image_trans.find_cut_in_find_ROI()
